9a56f2a9cec3cfa398710f2ff6326cbf79f13b02
Lukas Jaronis <jaronisluke13@gmail.com>
2020-04-17 19:51:50 -0400

    online and offline works with msg deletions

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # JaseBot
+
+setActivity can't be spammed due to API rate limits. Eventually the activity will change (doesn't take too long) when there is no more limits.
--- commands/online.js
+++ commands/online.js
@@ -9,17 +9,57 @@ client.on("message", async (message) => {
 
   // Fetching the role and awaiting the promise
   let JaseRole = await message.guild.roles.fetch("700754612138934502");
-  // Getting the roles id
   let getID = JaseRole.id;
+
+  // Finding the channel
+  let findChannel = client.channels.cache.find((ch) => ch.name === "sauti");
+  let sauti = findChannel.id;
 
   if (command === "online") {
     if (message.member.roles.cache.has(getID)) {
-      client.user.setActivity(`twitch`, {
-        type: "STREAMING",
-        url: "https://www.twitch.tv/tastejase",
+      client.user
+        .setActivity(`twitch`, {
+          type: "STREAMING",
+          url: "https://www.twitch.tv/tastejase",
+        })
+        .then((presence) =>
+          console.log(`Activity set to ${presence.activities[0].name}`)
+        )
+        .catch(console.error);
+      client.channels.cache
+        .get(`${sauti}`)
+        .send("@everyone Jason is streaming! https://www.twitch.tv/tastejase");
+
+      // Getting user ID
+      getUserID = message.member.user.id;
+      // Getting the user object
+      getUserObj = client.users.cache.get(getUserID);
+      // Getting the channel ID of last message sent of USER
+      getUserChannelID = getUserObj.lastMessageChannelID;
+      // Getting text channel the message was sent from
+      getLastMessageChannel = client.channels.cache.get(getUserChannelID);
+      // fetching last 5 messages
+      fetchMsg = await getLastMessageChannel.messages
+        .fetch({ limit: 5 })
+        .then((msg) => {
+          return msg;
+        });
+
+      // Mapping it out, slicing it up, and shifting it out of the array
+      fetchMsgMap = fetchMsg.map((item) => {
+        let sliced = item.content.slice(prefix.length).split(/ +/);
+        let shifted = sliced.shift().toLowerCase();
+        return shifted;
       });
+
+      // Filter through them
+      let filterEM = fetchMsgMap.filter((item) => item === "online");
+      console.log(filterEM);
+
+      // Finally delete these messages
+      message.delete({ timeout: 800 });
     } else {
-      message.author.send("Sorry, you don't have permissions for that!");
+      message.channel.send("Sorry, you don't have permissions for that!");
     }
   }
 });
