a6d10c13eea7d6dd5bccd7610ccf02d62ce0d4ef
lukas <jaronisluke13@gmail.com>
2020-04-18 12:42:49 -0400

    procfile added

---
+++ commands/prune.js
@@ -0,0 +1,33 @@
+const client = require("../index");
+const { prefix } = require("../config.json");
+const ms = require('ms');
+
+client.on("message", async (message) => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    let adminRole = await message.guild.roles.fetch("700888529000988683");
+    let getID = adminRole.id;
+
+    if (command === "prune") {
+        if (message.member.roles.cache.has(getID)) {
+            let person = message.guild.member(message.mentions.users.first())
+
+            // checking if the person is in our discord
+            if (!person) {
+                return message.reply("sorry, I couldn't find that user")
+            } else {
+                let messageCount = args[1]
+                if (!messageCount) {
+                    return message.reply('You need to provide a message count!')
+                }
+                console.log(person.lastMessageChannelID)
+
+            }
+        } else {
+            message.reply('You do not have this permission!')
+        }
+    }
+});
