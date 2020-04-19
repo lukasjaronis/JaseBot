a302d5768ff796f04e4797d652786bed51bae4ec
lukas <jaronisluke13@gmail.com>
2020-04-19 03:19:31 -0400

    full access permissions

---
+++ permissions.js
@@ -0,0 +1,9 @@
+// Discord Role ID's for full access
+
+// 700887245514866698 = Jase Role
+// 700888529000988683 = Adminstrator Role 
+// 700888657976098847 = Bot Mechanic
+
+const fullAccess = [700887245514866698, 700888529000988683, 700888657976098847]
+
+module.exports = fullAccess
