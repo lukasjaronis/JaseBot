9c82bbbbbe9983438611d451718aa20292f07679
Lukas Jaronis <jaronisluke13@gmail.com>
2020-06-13 17:12:15 -0400

    Update

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-const TwitchClient = require('twitch').default
-const WebHookListener = require('twitch-webhooks').default
-
 const fs = require('fs')
 const Discord = require('discord.js')
 const { config } = require('dotenv')
+const { streamCheck } = require('./streamCheck')
 const client = new Discord.Client()
 module.exports = client
 
@@ -11,47 +9,6 @@ module.exports = client
 config({
   path: __dirname + '/.env',
 })
-
-async function streamSubscribe() {
-  // testing
-  const userId = '238273761'
-
-  // const userId = 61050409
-  const clientId = process.env.TWITCH_CLIENT_ID
-  const clientSecret = process.env.TWITCH_CLIENT_SECRET
-  const twitchClient = TwitchClient.withClientCredentials(
-    clientId,
-    clientSecret
-  )
-
-  const listener = await WebHookListener.create(twitchClient, {
-    port: process.env.PORT || 80,
-    reverseProxy: { port: 443, ssl: true },
-  })
-  listener.listen()
-
-  let prevStream = null
-  const subscription = await listener.subscribeToStreamChanges(
-    userId,
-    async (stream) => {
-      if (stream) {
-        console.log(stream, 'stream')
-        if (!prevStream) {
-          console.log(
-            `${stream.userDisplayName} just went live with title: ${stream.title}`
-          )
-        }
-      } else {
-        // no stream, no display name
-        const user = await twitchClient.helix.users.getUserById(userId)
-        console.log(user, 'user')
-        console.log(`${user.displayName} just went offline`)
-      }
-      prevStream = stream
-    }
-  )
-  return subscription
-}
 
 // turns commands folder into the command collection
 client.commands = new Discord.Collection()
@@ -73,6 +30,6 @@ client.once('ready', () => {
     )
 })
 
-streamSubscribe()
+streamCheck()
 
 client.login(process.env.TOKEN)
---
+++ streamCheck.js
@@ -0,0 +1,45 @@
+const TwitchClient = require('twitch').default
+const WebHookListener = require('twitch-webhooks').default
+
+async function streamCheck() {
+  // testing
+  const userId = '238273761'
+
+  // const userId = 61050409
+  const clientId = process.env.TWITCH_CLIENT_ID
+  const clientSecret = process.env.TWITCH_CLIENT_SECRET
+  const twitchClient = TwitchClient.withClientCredentials(
+    clientId,
+    clientSecret
+  )
+
+  const listener = await WebHookListener.create(twitchClient, {
+    hostName: 'https://captainjason-discord-bot.herokuapp.com/',
+    port: process.env.PORT || 80,
+  })
+  listener.listen()
+
+  let prevStream = null
+  const subscription = await listener.subscribeToStreamChanges(
+    userId,
+    async (stream) => {
+      if (stream) {
+        console.log(stream, 'stream')
+        if (!prevStream) {
+          console.log(
+            `${stream.userDisplayName} just went live with title: ${stream.title}`
+          )
+        }
+      } else {
+        // no stream, no display name
+        const user = await twitchClient.helix.users.getUserById(userId)
+        console.log(user, 'user')
+        console.log(`${user.displayName} just went offline`)
+      }
+      prevStream = stream
+    }
+  )
+  return subscription
+}
+
+module.exports = { streamCheck }
