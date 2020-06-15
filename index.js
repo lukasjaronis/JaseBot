4d64cc2e1cc1ab763f51898a79494962ecb2b5bd
Lukas Jaronis <jaronisluke13@gmail.com>
2020-06-15 00:30:18 -0400

    .

--- index.js
+++ index.js
@@ -38,6 +38,26 @@ async function checkStream() {
     async (stream) => {
       if (stream) {
         if (!prevStream) {
+          const url = 'https://id.twitch.tv/oauth2/token'
+
+          const requestData = {
+            client_id: process.env.TWITCH_CLIENT_ID,
+            client_secret: process.env.TWITCH_CLIENT_SECRET,
+            grant_type: 'client_credentials',
+          }
+
+          const options = {
+            method: 'post',
+            headers: {
+              'content-type': 'application/json',
+            },
+            data: requestData,
+            url,
+          }
+
+          const { data } = await axios(options)
+          const { access_token } = data
+
           // Finding the channel
           let findChannel = client.channels.cache.find(
             (ch) => ch.name === post_channel
