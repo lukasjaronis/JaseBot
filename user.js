926d633c6163c4d3d9e24faffe96bf924ecf9d6b
Lukas Jaronis <jaronisluke13@gmail.com>
2020-06-15 23:42:40 -0400

    Final push

--- user.js
+++ user.js
@@ -1,18 +1,18 @@
 const hostname = '64.227.3.188'
 
 // Development
-const twitch_user_id = '238273761'
-const post_channel = 'testing'
-const post_channel_id = '701844825326092389'
-const detect_text = 'lukas'
-const twitch_url = 'https://www.twitch.tv/touchmybitcoin'
+// const twitch_user_id = '238273761'
+// const post_channel = 'testing'
+// const post_channel_id = '701844825326092389'
+// const detect_text = 'lukas'
+// const twitch_url = 'https://www.twitch.tv/touchmybitcoin'
 
-// Production
-// const twitch_user_id = '61050409'
-// const post_channel = "stream-info"
-// const post_channel_id = '700889883056799854'
-// const detect_text = '@everyone'
-// const twitch_url = "https://www.twitch.tv/tastejase"
+Production
+const twitch_user_id = '61050409'
+const post_channel = 'stream-info'
+const post_channel_id = '700889883056799854'
+const detect_text = '@everyone'
+const twitch_url = 'https://www.twitch.tv/tastejase'
 
 module.exports = {
   hostname,
