be8c35c2e79f1a85dac9f253dc4a5bb52ee697af
Lukas Jaronis <jaronisluke13@gmail.com>
2020-06-25 01:18:29 -0400

    Changed cmds to !rick, and fixed permissions

--- permissions.js
+++ permissions.js
@@ -1,20 +1,20 @@
 // Discord Role ID's for full access
 
 // 700887245514866698 = Jase Role
-// 700888529000988683 = Adminstrator Role 
+// 700888529000988683 = Adminstrator Role
 // 700888657976098847 = Bot Mechanic
 
-const permissions = ['700887245514866698', '700888529000988683', '700888657976098847']
-
+const permissions = [
+  '700887245514866698',
+  '700888529000988683',
+  '700888657976098847',
+]
 
 function accessCheck(message) {
-  
-        const accessCheck = permissions.map(items => {
-            // Checking if member has any of the provided roles inside the permissions array.
-            return message.member.roles.cache.has(items)
-        })
-        return accessCheck
+  const accessCheck = message.member.roles.cache.some((role) =>
+    permissions.includes(role.id)
+  )
+  return accessCheck
 }
-
 
 module.exports = accessCheck
