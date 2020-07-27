ff6f2de50cd6e17f799400074a6dde5e1b898c6e
lukasjaronis <jaronisluke13@gmail.com>
2020-07-27 13:15:42 -0400

    Update time for raffle from 1 minute to 4 hours

--- commands/raffle.js
+++ commands/raffle.js
@@ -14,8 +14,8 @@ client.on('message', async (message) => {
       if (adminCheck) {
         let item = args[0].split('"').join('')
 
-        // const time = 3600000
-        const time = 60000
+        // 4 hours
+        const time = 10800000
 
         const filter = (reaction, user) => {
           return reaction.emoji.name === '🥭' && user.id === message.author.id
@@ -28,7 +28,7 @@ client.on('message', async (message) => {
           message.delete().catch(e => console.log(e))
         } else {
           const raffleMessageSent = await message.channel.send(
-            `@everyone, ${message.author} has started a raffle! It will end in **1 minute**. If you want to participate hit the 🥭 \n\n This raffle is for ` +
+            `@everyone, ${message.author} has started a raffle! It will end in **4 hours**. If you want to participate hit the 🥭 \n\n This raffle is for ` +
             item
           ).catch(e => console.log(e))
 
