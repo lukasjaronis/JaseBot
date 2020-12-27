1e29ae589cb904658ec10d25a7646ed0bfce51a2
lukasjaronis <jaronisluke13@gmail.com>
2020-12-27 16:15:40 -0500

    changes

--- twitch.js
+++ twitch.js
@@ -30,10 +30,6 @@ const userId = twitch_user_id
 const clientId = process.env.TWITCH_CLIENT_ID
 const clientSecret = process.env.TWITCH_CLIENT_SECRET
 
-console.log(userId, 'id')
-console.log(clientId, 'client id')
-console.log(clientSecret, 'client secret')
-
 async function checkStream() {
   const authProvider = new ClientCredentialsAuthProvider(clientId, clientSecret);
   const apiClient = new ApiClient({ authProvider });
@@ -43,97 +39,109 @@ async function checkStream() {
     listenerPort: 8090
 }));
 
-  console.log(authProvider, 'auth provider?')
-
   listener.listen()
 
   let prevStream = await apiClient.helix.streams.getStreamByUserId(userId)
 
-  const subscription = await listener.subscribeToStreamChanges(
-    userId,
-    async (stream) => {
-      console.log(stream, 'stream')
-      if (stream) {
-        const { gameId } = stream
-
+  const subscription = await listener.subscribeToStreamChanges(userId, async (stream) => {
+    if (stream) {
         if (!prevStream) {
-          const token = await getToken()
-          const { access_token } = token
-
-          let findChannel = client.channels.cache.find(({ name }) => name == post_channel)
-          let streamInfo = findChannel.id
-
-          client.user
-            .setActivity('twitch ❤️', {
-              type: 'STREAMING',
-              url: 'https://www.twitch.tv/tastejase',
-            })
-            .then((presence) =>
-              console.log(`Activity set to ${presence.activities[0].name}`)
-            )
-            .catch(console.error)
-
-          const getGame = async (id) => {
-            const { data: results } = await axios.get(
-              `https://api.twitch.tv/helix/games?id=${id}`,
-              {
-                headers: {
-                  'content-type': 'application/json',
-                  'Client-Id': process.env.TWITCH_CLIENT_ID,
-                  Authorization: `Bearer ${access_token}`,
-                },
-              }
-            )
-            return results.data
-          }
-
-          const [game] = await getGame(gameId).catch(console.error)
-
-          let msg = liveEmbed(stream, game.name)
-
-          client.channels.cache.get(`${streamInfo}`).send(detect_text, msg)
-
-          client.channels.cache
-            .get(`${streamInfo}`)
-            .send(`Come watch! ${twitch_url}`)
+            console.log(`${stream.userDisplayName} just went live with title: ${stream.title}`);
         }
-      } else {
-        client.user
-          .setActivity('everybody', {
-            type: 'WATCHING',
-          })
-          .then((presence) =>
-            console.log(`Activity set to ${presence.activities[0].name}`)
-          )
-          .catch(console.error)
-
-        // Finding the channel
-        let findChannel = client.channels.cache.find(
-          (ch) => ch.name === post_channel
-        )
-
-        let streamInfo = findChannel.id
-
-        const user = await twitchClient.helix.users.getUserById(userId)
-        client.channels.cache
-          .get(`${streamInfo}`)
-          .send(`${user.displayName} just went offline.`)
-
-        // Get info of stream-info and handle message deletions
-        let getStreamInfo = client.channels.cache.get(post_channel_id)
-        await getStreamInfo.messages
-          .fetch({ limit: 30 })
-          .then((collected) => {
-            collected.forEach((msg) => {
-              msg.delete()
-            })
-          })
-          .catch((error) => console.log(error))
-      }
-      prevStream = stream
+    } else {
+        // no stream, no display name
+        const user = await apiClient.helix.users.getUserById(userId);
+        console.log(`${user.displayName} just went offline`);
     }
-  )
-  return subscription
+    prevStream = stream;
+})
+
+return subscription
+
+  // const subscription = await listener.subscribeToStreamChanges(
+  //   userId,
+  //   async (stream) => {
+  //     if (stream) {
+  //       const { gameId } = stream
+
+  //       if (!prevStream) {
+  //         const token = await getToken()
+  //         const { access_token } = token
+
+  //         let findChannel = client.channels.cache.find(({ name }) => name == post_channel)
+  //         let streamInfo = findChannel.id
+
+  //         client.user
+  //           .setActivity('twitch ❤️', {
+  //             type: 'STREAMING',
+  //             url: 'https://www.twitch.tv/tastejase',
+  //           })
+  //           .then((presence) =>
+  //             console.log(`Activity set to ${presence.activities[0].name}`)
+  //           )
+  //           .catch(console.error)
+
+  //         const getGame = async (id) => {
+  //           const { data: results } = await axios.get(
+  //             `https://api.twitch.tv/helix/games?id=${id}`,
+  //             {
+  //               headers: {
+  //                 'content-type': 'application/json',
+  //                 'Client-Id': process.env.TWITCH_CLIENT_ID,
+  //                 Authorization: `Bearer ${access_token}`,
+  //               },
+  //             }
+  //           )
+  //           return results.data
+  //         }
+
+  //         const [game] = await getGame(gameId).catch(console.error)
+
+  //         let msg = liveEmbed(stream, game.name)
+
+  //         client.channels.cache.get(`${streamInfo}`).send(detect_text, msg)
+
+  //         client.channels.cache
+  //           .get(`${streamInfo}`)
+  //           .send(`Come watch! ${twitch_url}`)
+  //       }
+  //     } else {
+  //       client.user
+  //         .setActivity('everybody', {
+  //           type: 'WATCHING',
+  //         })
+  //         .then((presence) =>
+  //           console.log(`Activity set to ${presence.activities[0].name}`)
+  //         )
+  //         .catch(console.error)
+
+  //       // Finding the channel
+  //       let findChannel = client.channels.cache.find(
+  //         (ch) => ch.name === post_channel
+  //       )
+
+  //       let streamInfo = findChannel.id
+
+  //       const user = await twitchClient.helix.users.getUserById(userId)
+  //       client.channels.cache
+  //         .get(`${streamInfo}`)
+  //         .send(`${user.displayName} just went offline.`)
+
+  //       // Get info of stream-info and handle message deletions
+  //       let getStreamInfo = client.channels.cache.get(post_channel_id)
+  //       await getStreamInfo.messages
+  //         .fetch({ limit: 30 })
+  //         .then((collected) => {
+  //           collected.forEach((msg) => {
+  //             msg.delete()
+  //           })
+  //         })
+  //         .catch((error) => console.log(error))
+  //     }
+  //     prevStream = stream
+  //   }
+  // )
+  // return subscription
 }
 
 client.login(process.env.TOKEN)
