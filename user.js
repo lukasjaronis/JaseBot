8882d63d8b85cf173835635980fd508052ac76dd
lukasjaronis <jaronisluke13@gmail.com>
2020-12-27 15:46:29 -0500

    Dev Mode

--- user.js
+++ user.js
@@ -3,18 +3,18 @@
 const hostname = '3.14.65.207'
 
 // Development
-// const twitch_user_id = '238273761'
-// const post_channel = 'testing'
-// const post_channel_id = '751244442446332054'
-// const detect_text = 'lukas'
-// const twitch_url = 'https://www.twitch.tv/touchmybitcoin'
+const twitch_user_id = '238273761'
+const post_channel = 'testing'
+const post_channel_id = '751244442446332054'
+const detect_text = 'lukas'
+const twitch_url = 'https://www.twitch.tv/touchmybitcoin'
 
 // Production
-const twitch_user_id = '61050409'
-const post_channel = 'stream-info'
-const post_channel_id = '700889883056799854'
-const detect_text = '@everyone'
-const twitch_url = 'https://www.twitch.tv/tastejase'
+// const twitch_user_id = '61050409'
+// const post_channel = 'stream-info'
+// const post_channel_id = '700889883056799854'
+// const detect_text = '@everyone'
+// const twitch_url = 'https://www.twitch.tv/tastejase'
 
 module.exports = {
   hostname,
