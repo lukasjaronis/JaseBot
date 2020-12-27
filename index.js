e19367592c94841208aa7a68f196f79157b36d29
lukasjaronis <jaronisluke13@gmail.com>
2020-12-27 00:49:44 -0500

    .

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ config({
 })
 
 client.commands = new Discord.Collection()
-const commandFiles = fs
-  .readdirSync('./commands')
-  .filter((file) => file.endsWith('.js'))
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`)
